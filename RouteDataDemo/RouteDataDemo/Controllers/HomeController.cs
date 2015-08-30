a6894f4788e437429f1dfb11c515ebdc826496d9
fnalin <fnalin@msn.com>
2015-08-30 09:07:35 -0300

    Add routedata logic

--- RouteDataDemo/RouteDataDemo/Controllers/HomeController.cs
+++ RouteDataDemo/RouteDataDemo/Controllers/HomeController.cs
@@ -10,20 +10,29 @@ namespace RouteDataDemo.Controllers
     {
         public ActionResult Index()
         {
+            dadosRota();
             return View();
+        }
+
+        private void dadosRota()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"] ?? "nulo";
+            ViewBag.Rota = string.Format("Controller: {0} | Action: {1} | ID: {2}", controller, action, id);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            dadosRota();
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            dadosRota();
             return View();
         }
     }
